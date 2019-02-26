fccbc11ac8763ab45862e18bcb466dc91e857416
fabio.mota <fabio.mota@hostelworld.com>
2019-02-26 15:24:56 +0000

    server.js

---
+++ express/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const serverless = require('serverless-http')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+const router = express.Router()
+
+router.get('/api/rooms', (req, res) => {
+  const rooms = {
+    rooms: [
+      {
+        beds: 1,
+        id: 1,
+        name: 'Basic 1 Bed Male Dorm',
+        type: 'dorm'
+      },
+      {
+        beds: 3,
+        id: 2,
+        name: 'Basic 3 Bed Male Dorm',
+        type: 'dorm'
+      },
+      {
+        beds: 2,
+        id: 3,
+        name: 'Private room',
+        type: 'private'
+      }
+    ]
+  }
+  res.send(rooms)
+})
+
+app.use('/.netlify/functions/server', router) // path must route to lambda
+// app.use('/', router)
+
+module.exports = app
+module.exports.handler = serverless(app)
